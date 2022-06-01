c52397490d440528a8d450a21f1ac4a01ac760ce
Anup Gupta <anup.cloud.devops@gmail.com>
2022-06-01 16:29:21 +1000

    Update Branch Name

--- hooks/branch_name.sh
+++ hooks/branch_name.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 local_branch="$(git branch --show-current)"
 
-valid_branch_regex="^(feat|bugfix|docs|style|release|hotfix|fix)\/[a-z|A-Z]+-[0-9]+$"
+valid_branch_regex="^(feat|bugfix|docs|style|release|hotfix|fix)\/[a-z|A-Z]+-[0-9]+|main|master$"
 
 message="There is something wrong with your branch name. Branch names in this project must adhere to this contract: $valid_branch_regex. Your commit will be rejected. You should rename your branch to a valid name and try again."
 
